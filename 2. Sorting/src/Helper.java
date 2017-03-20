0f2360f323d8dd242a5ccd11d8d7c1c3fd9b8a5d
GaPhil <gaphil.io@gmail.com>
2017-03-20 15:52:47 +0100

    add helper functions

---
+++ 2. Sorting/src/Helper.java
@@ -0,0 +1,27 @@
+public class Helper {
+
+    private static boolean less(Comparable x, Comparable y) {
+        return x.compareTo(y) < 0;
+    }
+
+    private static void exchange(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static void print(Comparable[] a) {
+        for (Comparable i : a) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            if (less(a[i], a[i - 1]))
+                return false;
+        }
+        return true;
+    }
+}
