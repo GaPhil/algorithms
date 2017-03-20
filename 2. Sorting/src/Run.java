b6bb62b389f6d2cb5e3d065eb70ed7c3896ec328
GaPhil <gaphil.io@gmail.com>
2017-03-20 18:19:08 +0100

    read strings

--- 2. Sorting/src/Run.java
+++ 2. Sorting/src/Run.java
@@ -1,16 +1,24 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Run {
+public abstract class Run {
 
     public static void main(String[] args) {
 
         Scanner reader = new Scanner(System.in);
-        System.out.println("Enter a number: ");
+        System.out.println("Enter the values and confirm with '#': ");
 
-        String[] a = null;
-        for (int i = 0; i < a.length; i++) {
-            a[i] = reader.next();
+        ArrayList<String> list = new ArrayList<>();
+
+        while (true) {
+            String key = reader.next();
+            char x = key.charAt(0);
+            int ascii = (int)x;
+            if (ascii == 35) break;
+            list.add(key);
         }
+
+        String[] a = list.toArray(new String[list.size()]);
         // call sorting algorithm
         assert Helper.isSorted(a);
         Helper.print(a);
