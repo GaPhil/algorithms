17a243b78673f8e05525bbe4050974416edca5ec
GaPhil <gaphil.io@gmail.com>
2017-03-28 16:24:24 +0200

    add BST.java

---
+++ 3. Searching/src/BST.java
@@ -0,0 +1,68 @@
+public class BST<Key extends Comparable<Key>, Value> {
+
+    private Node root;
+
+    private class Node {
+        private Key key;
+        private Value val;
+        private Node left;
+        private Node right;
+        private int n;
+
+        public Node(Key key, Value val, int n) {
+            this.key = key;
+            this.val = val = val;
+            this.n = n;
+        }
+    }
+
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node x) {
+        if (x == null) {
+            return 0;
+        } else {
+            return x.n;
+        }
+    }
+
+    public Value get(Key key) {
+        return get(root, key);
+    }
+
+    public Value get(Node x, Key key) {
+        if (x == null) {
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            return get(x.left, key);
+        } else if (cmp > 0) {
+            return get(x.right, key);
+        } else {
+            return x.val;
+        }
+    }
+
+    public void put(Key key, Value val) {
+        root = put(root, key, val);
+    }
+
+    private Node put(Node x, Key key, Value val) {
+        if (x == null) {
+            return new Node(key, val, 1);
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            x.left = put(x.left, key, val);
+        } else if (cmp > 0) {
+            x.right = put(x.right, key, val);
+        } else {
+            x.val = val;
+        }
+        x.n = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+}
