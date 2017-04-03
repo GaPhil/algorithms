c2afb8e8c03643330ab234704a8a4fa6e2dc973e
GaPhil <gaphil.io@gmail.com>
2017-04-03 13:55:23 +0200

    add SeparateChainingHashST

---
+++ 3. Searching/src/SeparateChainingHashST.java
@@ -0,0 +1,40 @@
+public class SeparateChainingHashST<Key, Value> {
+
+    private int m;                                  // hash table size
+    private SequentialSearchST<Key, Value>[] st;    // array of ST objects
+
+    public SeparateChainingHashST() {
+        this(997);
+    }
+
+    public SeparateChainingHashST(int m) {
+        // create m linked lists
+        this.m = m;
+        st = (SequentialSearchST<Key, Value>[]) new SequentialSearchST[m];
+        for (int i = 0; i < m; i++) {
+            st[i] = new SequentialSearchST();
+        }
+    }
+
+    private int hash(Key key) {
+        return (key.hashCode() & 0x7fffffff) % m;
+    }
+
+    public Value get(Key key) {
+        return (Value) st[hash(key)].get(key);
+    }
+
+    public void put(Key key, Value val) {
+        st[hash(key)].put(key, val);
+    }
+
+    // return keys in symbol table as an Iterable
+    public Iterable<Key> keys() {
+        Queue<Key> queue = new Queue<Key>();
+        for (int i = 0; i < m; i++) {
+            for (Key key : st[i].keys())
+                queue.enqueue(key);
+        }
+        return queue;
+    }
+}
