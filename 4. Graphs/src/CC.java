9ba785681a171b0955b479f49e518931fea66ec1
GaPhil <gaphil.io@gmail.com>
2017-04-03 18:53:21 +0200

    add Connected components

---
+++ 4. Graphs/src/CC.java
@@ -0,0 +1,38 @@
+public class CC {
+
+    private boolean[] marked;
+    private int[] id;
+    private int count;
+
+    public CC(Graph G) {
+        marked = new boolean[G.V()];
+        for (int s = 0; s < G.V(); s++) {
+            if (!marked[s]) {
+                dfs(G, s);
+                count++;
+            }
+        }
+    }
+
+    private void dfs(Graph G, int v) {
+        marked[v] = true;
+        id[v] = count;
+        for (int w : G.adj(v)) {
+            if (!marked[w]) {
+                dfs(G, w);
+            }
+        }
+    }
+
+    public boolean connected(int v, int w) {
+        return id[v] == id[w];
+    }
+
+    public int id(int v) {
+        return id[v];
+    }
+
+    public int count() {
+        return count;
+    }
+}
