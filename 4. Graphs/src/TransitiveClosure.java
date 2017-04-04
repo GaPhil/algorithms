780476165dd216ce07d7dbb70922bab6e5417fbc
GaPhil <gaphil.io@gmail.com>
2017-04-04 14:03:33 +0200

    add TransitiveClosure

---
+++ 4. Graphs/src/TransitiveClosure.java
@@ -0,0 +1,15 @@
+public class TransitiveClosure {
+
+    private DirectedDFS[] all;
+
+    TransitiveClosure(Digraph G) {
+        all = new DirectedDFS[G.V()];
+        for (int v = 0; v < G.V(); v++) {
+            all[v] = new DirectedDFS(G, v);
+        }
+    }
+
+    boolean reachable(int v, int w) {
+        return all[v].marked(w);
+    }
+}
