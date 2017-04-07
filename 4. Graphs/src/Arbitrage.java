21655d82b7371048cb3867d91d94c9f4acc61d48
GaPhil <gaphil.io@gmail.com>
2017-04-07 16:08:44 +0200

    add Arbitrage

---
+++ 4. Graphs/src/Arbitrage.java
@@ -0,0 +1,31 @@
+import edu.princeton.cs.introcs.StdIn;
+import edu.princeton.cs.introcs.StdOut;
+
+public class Arbitrage {
+
+    public static void main(String[] args) {
+        int V = StdIn.readInt();
+        String[] name = new String[V];
+        EdgeWeightedDigraph G = new EdgeWeightedDigraph(V);
+        for (int v = 0; v < V; v++) {
+            name[v] = StdIn.readString();
+            for (int w = 0; w < V; w++) {
+                double rate = StdIn.readDouble();
+                DirectedEdge e = new DirectedEdge(v, w, -Math.log(rate));
+                G.addEdge(e);
+            }
+        }
+
+        BellmanFordSP spt = new BellmanFordSP(G, 0);
+        if (spt.hasNegativeCycle()) {
+            double stake = 1000.0;
+            for (DirectedEdge e : spt.negativeCycle()) {
+                StdOut.printf("%10.5f % s ", stake, name[e.from()]);
+                stake *= Math.exp(-e.weight());
+                StdOut.printf("= %10.5f %s\n", stake, name[e.to()]);
+            }
+        } else {
+            StdOut.printf("No arbitrage opportunity");
+        }
+    }
+}
