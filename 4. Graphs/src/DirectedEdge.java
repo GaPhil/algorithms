e1044f2ee83606937fc22a233a123b097a586295
GaPhil <gaphil.io@gmail.com>
2017-04-07 09:34:12 +0200

    add DirectedEdge

---
+++ 4. Graphs/src/DirectedEdge.java
@@ -0,0 +1,28 @@
+public class DirectedEdge {
+
+    private final int v;                // edge tail
+    private final int w;                // edge head
+    private final double weight;        // edge weight
+
+    public DirectedEdge(int v, int w, double weight) {
+        this.v = v;
+        this.w = w;
+        this.weight = weight;
+    }
+
+    public double weight() {
+        return weight;
+    }
+
+    public int from() {
+        return v;
+    }
+
+    public int to() {
+        return w;
+    }
+
+    public String toString() {
+        return String.format("%d->%d %.2f", v, w, weight);
+    }
+}
