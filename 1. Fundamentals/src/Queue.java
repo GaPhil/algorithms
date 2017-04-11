2597c0aefc2be897cb89be271e6e765b35b01a45
GaPhil <gaphil.io@gmail.com>
2017-04-11 11:11:38 +0200

    update Queue

--- 1. Fundamentals/src/Queue.java
+++ 1. Fundamentals/src/Queue.java
@@ -1,3 +1,5 @@
+import java.util.Iterator;
+
 public class Queue<Item> implements Iterable<Item> {
 
     private Node first;                 // link to least recently added node
@@ -10,7 +12,58 @@ public class Queue<Item> implements Iterable<Item> {
         Node next;
     }
 
+    public boolean isEmpty() {
+        return first == null;
+    }
 
+    public int size() {
+        return n;
+    }
 
+    public void enqueue(Item item) {
+        // Add item to the end of the list.
+        Node oldlast = last;
+        last = new Node();
+        last.item = item;
+        last.next = null;
+        if (isEmpty()) {
+            first = last;
+        } else {
+            oldlast.next = last;
+        }
+        n++;
+    }
 
+    public Item dequeue() {
+        // Remove item from the beginning of the list.
+        Item item = first.item;
+        first = first.next;
+        n--;
+        if (isEmpty()) {
+            last = null;
+        }
+        return item;
+    }
+
+    public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<Item> {
+        private Node current = first;
+
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public void remove() {
+
+        }
+
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
 }
